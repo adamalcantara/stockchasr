3ab6f89b89e5a1744d6ee08cf473c9af254d921b
adamalcantara <alcantaradevelopment@gmail.com>
2021-05-26 21:13:07 -0400

    Added findstock to the container

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -20,6 +20,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { mainListItems, secondaryListItems } from './listItems';
+import FindStock from "../components/FindStock";
 // import Chart from './Chart';
 import Deposits from './Deposits';
 import Orders from './Orders';
@@ -168,41 +169,7 @@ function DashboardContent() {
       >
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-          <Grid container spacing={3}>
-            {/* Chart */}
-            <Grid item xs={12} md={8} lg={9}>
-              <Paper
-                sx={{
-                  p: 2,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  height: 240,
-                }}
-              >
-                {/* <Chart /> */}
-              </Paper>
-            </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper
-                sx={{
-                  p: 2,
-                  display: 'flex',
-                  flexDirection: 'column',
-                  height: 240,
-                }}
-              >
-                <Deposits />
-              </Paper>
-            </Grid>
-            {/* Recent Orders */}
-            <Grid item xs={12}>
-              <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                <Orders />
-              </Paper>
-            </Grid>
-          </Grid>
-          <Copyright sx={{ pt: 4 }} />
+           <FindStock />
         </Container>
       </Box>
     </Box>
