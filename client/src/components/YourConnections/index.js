94e8d1160ccd128ba27ef616e12c3135896e4a50
adamalcantara <alcantaradevelopment@gmail.com>
2021-05-27 14:37:17 -0400

    Dashboard contains logo

---
+++ client/src/components/YourConnections/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const YourConnections = () => {
+    return (
+        <div>
+            <h1>You have reached Your Connections</h1>
+        </div>
+    )
+}
+
+export default YourConnections
