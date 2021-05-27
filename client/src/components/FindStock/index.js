defdce46b4c2b988e7ab74f12058731c2f78214a
Arecio Canton <cuba289@gmail.com>
2021-05-26 21:55:52 -0400

    added search bar

---
+++ client/src/components/FindStock/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FindStock = () => {
+    return (
+        <div>
+            <input type='text' placeholder='Search'></input>
+            <button className="btn btn-primary">Search</button>
+        </div>
+    )
+}
+
+export default FindStock
