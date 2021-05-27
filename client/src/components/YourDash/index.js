43a3879c96c9689527229fde688097fc73a47034
adamalcantara <alcantaradevelopment@gmail.com>
2021-05-27 14:12:46 -0400

    New dashboard working

---
+++ client/src/components/YourDash/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FindStock = () => {
+    return (
+        <div>
+            <h1>You have reached Your Dashboard</h1>
+        </div>
+    )
+}
+
+export default FindStock
