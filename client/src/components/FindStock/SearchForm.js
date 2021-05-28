63cfd37f5a624088e91544e644bb4bc745caa004
Arecio Canton <cuba289@gmail.com>
2021-05-28 14:03:54 -0400

    appending ticker symbol, description, name, url, logo

--- client/src/components/FindStock/SearchForm.js
+++ client/src/components/FindStock/SearchForm.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const SearchForm = ({ getStock }) => {
+const SearchForm = ({ getStock, getStockInfo }) => {
     return (
         <div>
             <h1>You have reached the FindStock element</h1>
             <input type='text' placeholder='Search'></input>
-            <button className="ml-3 btn btn-primary" onClick={(e) => getStock(e)}>Search</button>
+            <button className="ml-3 btn btn-primary" onClick={(e) => getStockInfo(e)} >Search</button>
         </div>
     )
 }
