efd59815aace4b6a6b2086063cafe24ab75d9d19
mooreFL <moorepr09@gmail.com>
2021-06-03 17:07:55 -0400

    comments now appended to page

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -1,76 +1,74 @@
-import React, {useRef, useContext, useState, useEffect } from "react";
+import React, { useRef, useContext, useState, useEffect } from "react";
 import API from "../../utils/API";
-import { UserContext } from "../../utils/UserContext"
-
+import { UserContext } from "../../utils/UserContext";
 
 function CommentForm({ searchValue }) {
-    const input = useRef()
-    const [commentList, setCommentList] = useState([]);
+  const input = useRef();
+  const [commentList, setCommentList] = useState([]);
 
-    console.log("The hills are a live with the sound of music")
-    console.log(searchValue)
+  console.log("The hills are a live with the sound of music");
+  console.log(searchValue);
 
-    const [user, dispatch] = useContext(UserContext);
-    
-    React.useEffect(() => getAllComments(), [])
+  const [user, dispatch] = useContext(UserContext);
 
-    function getAllComments () {
-        console.log("Alligators cant get aids")
-        API.getComment({searchValue}).then((res) => {
-            console.log(res.data);
-            var commentList = [];
-            console.log("This is from the get all comments function");
-            console.log(commentList);
-            // for (var i = 0; i < res.data.length; i++) {
-            //     commentList.push({
-            //       username: res.data[i],
-            //       comment: res.data[i],
-            //     });
-            //   }
-              setCommentList(commentList)
-        })
-    }
-    // getAllComments();
+  React.useEffect(() => getAllComments(), []);
 
-    return (
-        <div>
-            <input type="text" placeholder="Comment" ref={input}></input>
-            <button className="ml-3 btn btn-primary" onClick={(e) => API.addComment({
-                comments: input.current.value,
-                username: user.username,
-                stock: searchValue
-                })}>Add Comment</button>
-                {/* <button onClick={(getAllComments)}>Get Comments</button>  */}
-        
-            <table id="comment-list">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>UserName</th>
-                            <th>Comments</th>
-                        </tr>
-                    </thead>
-                    {/* <tbody>
-                        {getAllComments.map((stock) => {
-                            console.log(stock)
-                            return (
-                                <tr>
-                                    <td>
-                                    {getAllComments.username}
-                                    </td>
-                                    <td>
-                                    {getAllComments.comment}
-                                    </td>
-                                </tr>
-                            )
-                        })}
-                    </tbody> */}
-                </table>
-        </div>
-                
+  function getAllComments() {
+    console.log("Alligators cant get aids");
+    API.getComment({ searchValue }).then((res) => {
+      console.log(res.data);
+      var commentListArr = [];
+      console.log("This is from the get all comments function");
+      console.log(commentListArr);
+      for (var i = 0; i < res.data.length; i++) {
+        commentListArr.push({
+          username: res.data[i].username,
+          comment: res.data[i].comments,
+        });
+      }
+      setCommentList(commentListArr);
+    });
+  }
+  // getAllComments();
 
-    )
+  return (
+    <div>
+      <table id="comment-list">
+        {/* <thead>
+          <tr>
+            <th></th>
+            <th>UserName</th>
+            <th>Comments</th>
+          </tr>
+        </thead> */}
+        <tbody>
+          {commentList.map((stock, i) => {
+            console.log(stock);
+            return (
+              <tr>
+                <td>{stock.username} says:</td>
+                <td>{stock.comment}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <input type="text" placeholder="Comment" ref={input}></input>
+      <button
+        className="ml-3 btn btn-primary"
+        onClick={(e) =>
+          API.addComment({
+            comments: input.current.value,
+            username: user.username,
+            stock: searchValue,
+          })
+        }
+      >
+        Add Comment
+      </button>
+      {/* <button onClick={(getAllComments)}>Get Comments</button>  */}
+    </div>
+  );
 }
 
-
-export default CommentForm 
+export default CommentForm;
